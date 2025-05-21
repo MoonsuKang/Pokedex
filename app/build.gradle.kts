b3b08f2d60dedd7e7f6b9679567c8127749b883f
MoonsuKang <rkdms4971@naver.com>
2025-05-21 15:17:03 +0900

    update build configuration and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,17 @@
+import java.util.Properties
+
+val properties = Properties().apply {
+    load(project.rootProject.file("local.properties").inputStream())
+}
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -13,9 +23,9 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
-
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "BASE_URL", "\"${properties["base.url"]}\"")
     }
 
     buildTypes {
@@ -35,25 +45,41 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    // Test
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.bundles.test)
+
+    // Debug
+    debugImplementation(libs.bundles.debug)
+
+    // Androidx
+    implementation(libs.bundles.androidx)
+    implementation(platform(libs.compose.bom))
+    ksp(libs.room.compiler)
+
+    // Coroutine
+    implementation(libs.bundles.coroutines)
+
+    // DI
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
+
+    // Third-Party
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.bundles.okhttp)
+    implementation(libs.bundles.retrofit)
+    implementation(libs.kotlinx.serialization.json)
+
+    // ETC
+    implementation(libs.timber)
+    implementation(libs.lottie.compose)
+    implementation(libs.coil)
+    implementation(libs.compose.shimmer)
 }
